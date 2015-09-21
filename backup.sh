7c15398f9a37d38e05753b5bbc2a1b530221cb39
Jung Se Yeon <devhackin@gmail.com>
2015-09-21 17:15:01 +0900

    chore(auto): 2015-09-21 17:15:01 자동커밋을 하다

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ rsync -avPz --delete --stats -l -t /home/jsy/test/c1/ /home/jsy/test/c2/
 
 if [ $? -eq 0 ]
 then
+	cd /home/jsy/test;
 	git add --all;
 	git commit -m "chore(auto): ${DATE} 자동커밋을 하다";
 	git push;
